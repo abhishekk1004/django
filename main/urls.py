2016c6a71b53f14dcd48dd4c70bb5b30ab0b8c74
abhishekk1004 <abhishekkushwaha.np@gmail.com>
2025-09-15 11:01:00 +0545

    urls

--- main/urls.py
+++ main/urls.py
@@ -23,8 +23,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('product.urls')),
+    path('product/', include('product.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
